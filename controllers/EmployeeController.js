f0675becf22a3c3e25367e8facfe28f5795c5b0e
srivensy <srivensyshortterm@gmail.com>
2026-01-02 20:39:01 +0530

    get employee_by_id,update,delete API's added

--- controllers/EmployeeController.js
+++ controllers/EmployeeController.js
@@ -20,7 +20,6 @@ export const employeeController = {
       res.status(500).json({ message: "Server error" })
     }
   },
-
   getEmployee: async(req,res)=>{
     try{
       const employee = await Employee.find()
@@ -28,6 +27,42 @@ export const employeeController = {
     }catch{
       console.error("This is an error",error)
       res.status(500).json({message:"servererror"})
+    }
+  },
+  getsingleEmployee:async(req,res)=>{
+    try{
+      const employee = await Employee.findById(req.params.id)
+      if(!employee){
+        return res.status(404).json({message:"Employee not found"})
+      }
+      res.status(200).json(employee)
+    }catch(error){
+      console.log("there is an error occured",error)
+      res.status(500).json({message:"backend error"})
+    }
+  },
+  updateEmployee:async(req,res)=>{
+    try{
+      const {name,email,phone,city}=req.body
+
+      const employee = await Employee.findByIdAndUpdate(req.params.id,{name,email,phone,city})
+        
+      if(!employee){
+        return res.status(404).json({message:"employee not found"})
+      }
+      res.status(200).json(employee)
+    }catch(error){
+      console.log("there is a backend error",error)
+      res.status(500).json({message:"backend error (server"})
+    }
+  },
+  deleteEmployee:async(req,res)=>{
+    try{
+      const delete_employee=await Employee.findByIdAndDelete(req.params.id)
+      res.status(204).send(delete_employee)
+    }catch(error){
+      console.error("there is no data found:",error)
+      res.status(500).json({message:"no data found in the server"})
     }
   }
  
--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -5,5 +5,8 @@ const router = express.Router()
 
 router.post("/add_employee", employeeController.createEmployee)
 router.get("/get_all_employee",employeeController.getEmployee)
+router.get("/employee/:id",employeeController.getsingleEmployee)
+router.put('/update/:id',employeeController.updateEmployee)
+router.delete('/delete/:id',employeeController.deleteEmployee)
 
 export default router
